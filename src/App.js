cc2468c2256e58b51ab4efbc124885e9452ba201
simpleton <chazesingh@gmail.com>
2024-04-02 01:58:31 +0530

    INIT: completeTodo method to mark task as done, ADD: taskdone to the data -> structure

--- src/App.js
+++ src/App.js
@@ -7,15 +7,24 @@ import TaskForm from './components/TaskForm';
 function App() {
   //add state to handle tasks
   const [tasks,setTasks]=useState([
-    { text: "cook dinner" },
-    { text: "take walk after dinner" },
-    { text: "brush before sleep" }
+    { text: "cook dinner",taskDone: false },
+    { text: "take walk after dinner",taskDone: false },
+    { text: "brush before sleep",taskDone: false }
   ]);
   //New tasks with the text form current task added.
   const addNewTask= text=>{
     //create new array using spread and add text of new task
     const newTasks=[...tasks,{text}];
     //update the tasks state with new Tasks
+    setTasks(newTasks);
+  }
+  //function to handle completed tasks
+  const completTodo=index=>{
+    //copy the existing tasks array
+    const newTasks=[...tasks];
+    // Mark the task at  index to done
+    newTasks[index].taskDone=true;
+    //update the task using useState
     setTasks(newTasks);
   }
   return (
